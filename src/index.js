fcf85add4cff56427b14b2aca7d70956425008de
CompChimera <computationalchimera@gmail.com>
2018-11-19 16:21:02 -0400

    Game history, history visibility toggle, and style updates added

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-// SQUARE COMPONENT
-// Render's a button, which will be a square of the board
+import Button from '@material-ui/core/Button';
 function Square(props) {
-    return (
-      <button className="square" 
-        onClick={props.onClick}
-      >
-        {props.value}
-      </button>
-    );
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
-
-// BOARD COMPONENT
-//The whole board
 class Board extends React.Component {
   renderSquare(i) {
     return (
-      <Square 
-      value={this.props.squares[i]}
-      onClick={() => this.props.onClick(i)}
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
@@ -50,73 +43,106 @@ class Board extends React.Component {
   }
 }
 
-
-//GAME COMPONENT
 class Game extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
       xIsNext: true,
-    }
+      isHidden: true
+    };
   }
-
+  toggleHidden () {
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+  }
   handleClick(i) {
-    //We call slice to create a copy of the squares aray to modify. 
-    //This has to do with Immutability.
-    //We're not directly changing the main data.
-    //In this example, we can keep a history of moves in the main data
-    //Detect Changes
-    // 
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    
-    if ( calculateWinner(squares) || squares[i]){
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      xIsNext: !this.state.xIsNext,
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
     });
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
 
   render() {
-
     const history = this.state.history;
-    const current = history[history.length -1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
-    if (winner){
-      status = 'Winner: ' + winner;
+    if (winner) {
+      status = "Winner: " + winner;
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
 
+    const Child = () => (
+      <ol>{moves}</ol>
+    )
     return (
       <div className="game">
         <div className="game-board">
-          <Board 
+          <Board
             squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+            onClick={i => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
-          <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <div className="status">{status}</div>
+            <Button onClick={this.toggleHidden.bind(this)} variant="contained" color="primary">
+              {this.state.isHidden && "Show history"}
+              {!this.state.isHidden && "Hide history"}
+            </Button>
+            {/* {!this.state.isHidden && <Child moves={moves}/>} */}
+            {!this.state.isHidden && <Child />}
+          
         </div>
       </div>
     );
+
+    
   }
 }
+
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("game"));
 
 function calculateWinner(squares) {
   const lines = [
@@ -127,7 +153,7 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
+    [2, 4, 6]
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
@@ -137,10 +163,3 @@ function calculateWinner(squares) {
   }
   return null;
 }
-
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
